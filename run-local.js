534334787003b5dfe09359af353dc6ba52feaaed
Abhishek Gupta <168064482+AbhishekGupta-Landmark@users.noreply.github.com>
2025-10-03 21:24:49 +0530

    Clean up debug and local dev files

--- run-local.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-// Simple script to run the server without cross-env dependency issues
-process.env.NODE_ENV = 'development';
-
-// Import and run the server
-import('./server/index.ts').catch(console.error);
